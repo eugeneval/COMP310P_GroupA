912ca5c0d234aa6bf73a68dea41f0944f87c8688
Syed Ahmad <Ahmad@MacBook-Pro-3.local>
2017-11-09 20:30:47 +0000

    Changed Background Colour

--- website/main.php
+++ website/main.php
@@ -5,7 +5,7 @@
         <title>Eventi</title>
     </head>
     <body>
-      <script src="libraries/p5.js"></script>
+      <script src="libraries/p5.min.js"></script>
       <script src="javascript/main1.js"></script>
     </body>
 </html>
