2544ca809a706c13887153f6553bc58ab2e5e9b1
eugeneval <eugval@gmail.com>
2017-11-28 12:15:54 +0000

    Created basic page for all wireframe pages

---
+++ website/event_details.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Eventi - Event Details</title>
+        <!-- TODO: make title reflect currently selected event -->
+    </head>
+    <body>
+        <header>
+            <img src="resources/Logo.png" style="width:302px;height:86px;"/>
+            <h4>Welcome to Eventi, the intelligent assistant for young professionals!</h4>
+        </header>
+
+    </body>
+</html>
