b2a5bb70069327cb8a1db683c11f68b42f73b513
eugeneval <eugval@gmail.com>
2017-12-07 15:26:07 +0000

    Icons implemented NOTE: Image sizing currently in <style> as for some reason it doesn't work in styling.css

--- website/create_new_user.php
+++ website/create_new_user.php
@@ -4,6 +4,13 @@
         <meta charset="utf-8">
         <title>Eventi - New User</title>
         <link href="styling.css" rel="stylesheet">
+        <style>
+        /**/ NOTE: for some reason this stops working if you move it to styling.css.
+        /**/ TODO: figure out why.
+        img.form_icons {
+            height: 1em;
+        }
+        </style>
     </head>
     <body>
         <header>
@@ -11,23 +18,32 @@
             <h4>Welcome to Eventi, the intelligent assistant for young professionals!</h4>
         </header>
         <form method="post" action="login.php">
-            <h3>Or create a new account:</h3>
+            <h3>Create a new account:</h3>
+            <img src="resources/form_icons/Name.png" class="form_icons"/>
             <input type="text" placeholder="Full Name" maxlength="40" required name="name"/>
             <br />
+            <img src="resources/form_icons/Username.png" class="form_icons"/>
             <input type="text" placeholder="Chosen Username" maxlength="20" required name="usernameNew"/>
             <br />
+            <img src="resources/form_icons/Password.png" class="form_icons"/>
             <input type="password" placeholder="Password" maxlength="20" required name="password" id="password" onchange='check_pass();'/>
             <br />
+            <img src="resources/form_icons/Password.png" class="form_icons"/>
             <input type="password" placeholder="Confirm Password" maxlength="20" required name="passwordConfirm" id="confirm_password" onchange='check_pass();'/>
             <br />
+            <img src="resources/form_icons/Email.png" class="form_icons"/>
             <input type="email" placeholder="Email Address" maxlength="20" required name="email"/>
             <br />
+            <img src="resources/form_icons/Address.png" class="form_icons"/>
             <input type="text" placeholder="Company Address" required name="address"/>
             <br />
+            <img src="resources/form_icons/Work.png" class="form_icons"/>
             <input type="text" placeholder="Company Name" maxlength="20" required name="company"/>
             <br />
+            <img src="resources/form_icons/Phone.png" class="form_icons"/>
             <input type="tel" placeholder="Phone Number" maxlength="12" required name="phone"/>
             <br />
+            <img src="resources/form_icons/Paypal.png" class="form_icons"/>
             <input type="text" placeholder="Paypal" name="paypal"/>
             <br />
             <input type="submit" id="submit" disabled/>
