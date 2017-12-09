86161400ffc69180e0f24b288f7f23f19ceb8cb1
gimafidon <gimafidon@motivez.co.uk>
2017-12-09 18:13:14 +0000

    Styling for User Profile

--- website/user_profile.php
+++ website/user_profile.php
@@ -23,6 +23,20 @@ $username = checkCurrentUser();
                 <li class="menubar"><a href="login.php">Logout</a></li>
             </ul>
         </header>
+        <h3>Profile</h3>
+        <!--Store and Fetch Profile Picture in PHP and SQL-->
+        <h4>Edit Interests</h4>
+        <h4>Attending</h4>
+
+        <h4>Email Notifications</h4>
+        <label class="switch">
+            <input type="checkbox" checked>
+            <span class="slider round"></span>
+        </label>
+        <br /><br />
+        <button onclick="location.href = 'manage_event.php';" id="button" >Manage Events</button>
+        <button onclick="location.href = 'create_event.php';" id="button" >Create Events</button>
+        <button onclick="location.href = 'login.php';"id="button" >Log Out</button>
 
     </body>
 </html>
