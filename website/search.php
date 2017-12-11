b6c9987b198cd5661ac8d786d7f50fb97ff94bbe
gimafidon <gimafidon@motivez.co.uk>
2017-12-11 16:34:34 +0000

    Search Function - Nearly Complete

--- website/search.php
+++ website/search.php
@@ -3,6 +3,26 @@
 require 'functions.php';
 $username = checkCurrentUser();
 
+$conn = db_connect();
+$output = '';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $searchq = test_input($_POST["searchq"]);
+    $searchq = preg_replace("#[^0-9a-z]#i","",$searchq);
+
+    $query = mysqli_query($conn, "SELECT * FROM events WHERE Name LIKE '%$searchq%';");
+        $count = mysqli_num_rows($query);
+        if($count == 0) {
+            $output = 'No Results';
+        }
+        else{
+          while($row = mysqli_fetch_array($query)) {
+            $name = $row['Name'];
+
+            $output .= '<div>'.$name.' </div>';
+          }
+        }
+  }
 
 ?>
 
@@ -24,27 +44,11 @@ $username = checkCurrentUser();
         </header>
         <form action="search.php" method="POST">
             <input type="text" name="keywords" placeholder="Search for Event.." autocomplete="off" />
-            <input type="submit" value="search" />
+            <input type="submit" value="Search" />
 
           <?php
-          $conn = db_connect();
+              echo $output;
 
-          if(isset($_POST['search'])) {
-            $searchq = $_POST['search'];
-            $sql = "SELECT * FROM events WHERE Name LIKE '%$searchq%'"}
-            $result = mysqli_query($conn, $sql);
-            if (mysqli_num_rows($result) > 0) {
-                while($row = mysqli_fetch_assoc($result)) {
-                    echo "<p>".$row['Name']."<br /><small>".$row['Description']."</small></p>";
-                    echo "<form action='event_details.php' onclick=\"return  eventCookie(".$row['Event_ID'].")\">
-                    <input type=\"submit\" value=\"Event Details\" />
-                    </form>";
-                }
-            }
-            else {
-                echo "0 results";
-            }
-            mysqli_close();
           ?>
 
         </form>
