a31b8c35ad29e2477ae854d24abb3cbf347719ec
eugeneval <eugval@gmail.com>
2017-12-11 17:10:09 +0000

    ticket info from SQL

--- website/buy_tickets.php
+++ website/buy_tickets.php
@@ -11,6 +11,15 @@ if (!$event_ID) {
     exit();
 }
 
+$conn = db_connect();
+$sql = "SELECT e.Name, v.Name AS 'Venue_Name', e.Ticket_Price, e.Total_Tickets, COUNT(t.Ticket_ID) AS 'Tickets_Sold' FROM events e JOIN venue v ON v.Venue_ID = e.Venue_ID JOIN tickets t ON t.Event_ID = e.Event_ID WHERE e.Event_ID = '$event_ID'";
+$result = mysqli_query($conn, $sql);
+$row = mysqli_fetch_assoc($result);
+
+if (mysqli_num_rows($result) == 0) {
+    $eventCreation .= "This event doesn't exist!";
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -29,6 +38,48 @@ if (!$event_ID) {
                 <li class="menubar"><a href="login.php">Logout</a></li>
             </ul>
         </header>
-        <?php echo $event_ID; ?>
+        <div id = event_details_nv style="color: #4CAF50;">
+          </br><h2><?php echo $row['Name'];?></h2>
+            <h3>At</h3>
+          <h3><?php echo $row['Venue_Name'];?></h3>
+        </div>
+      </br>
+    <!-- <h3>Ticket Type</h3> -->
+      <form action="/action_page.php">
+          <!-- <select name="Ticket Type">
+              <option value="VIP">VIP</option>
+                <option value="Standard">Standard</option>
+                  <option value="Earlybird">Earlybird</option>
+            </select>
+          <br><br>
+             <h3>Quantity</h3>
+                <input type="number" name="quantity" min="1" max="5">
+             <h3>Email</h3>
+            <input type="email" name="email">
+          <br><br>
+                First name:<br>
+            <input type="text" name="firstname" value="First Name">
+          <br>
+              Last name:<br>
+            <input type="text" name="lastname" value="Last Name">
+          <br><br> -->
+          <p>Price per ticket: £<?php echo $row['Ticket_Price']; ?></p>
+          <p>Tickets Remaining: <?php echo ($row['Total_Tickets'] - $row['Tickets_Sold']); ?></p>
+          <p>Select quantity: </p>
+          <input type="number" name="quantity" id="quantity" value="0" max="<?php echo ($row['Total_Tickets'] - $row['Tickets_Sold']); ?>" onchange="ticketPrice()"><br />
+          <p id='total'>Your total: £0</p>
+
+        <input type="submit" value="Buy Tickets">
+      </form>
+
     </body>
+    <script>
+    ticket_price = <?php echo $row['Ticket_Price'] ?>;
+
+    function ticketPrice() {
+        total = ticket_price * document.getElementById('quantity').value;
+        document.getElementById('total').innerHTML = "Your total: £" + total;
+    }
+
+    </script>
 </html>
