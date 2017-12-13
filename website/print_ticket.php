b1b0860fecccc4f4fa5468037c823ea2be14bb56
Syed Ahmad <Ahmad@eduroam-int-dhcp-97-222-46.ucl.ac.uk>
2017-12-13 16:18:14 +0000

    Fixed minor issues

--- website/print_ticket.php
+++ website/print_ticket.php
@@ -3,22 +3,30 @@ require 'functions.php';
 require('libraries/fpdf/fpdf.php');
 $ticket_ID = $_COOKIE["ticket"];
 
+
 $conn = db_connect();
 
-$sql = "SELECT events.Name, events.Start_DateTime, venue.Name AS VenueName
-        FROM events
-        JOIN tickets ON events.Event_ID=tickets.Event_ID
-        JOIN venue ON events.Venue_ID=venue.Venue_ID
-        WHERE Ticket_ID = $ticket_ID;";
+
+
+$sql = "SELECT e.Name, e.Start_DateTime, v.Name AS 'VenueName'
+        FROM events e
+        JOIN tickets t ON e.Event_ID = t.Event_ID
+        JOIN venue v ON e.Venue_ID = v.Venue_ID
+        WHERE t.Ticket_ID = $ticket_ID;";
 
 $result = mysqli_query($conn, $sql);
 $row = mysqli_fetch_assoc($result);
-mysqli_close($conn);
 
+$rowd = 1200;
+
+die(print_r($rowd));
+
+mysqli_close($conn);
+/*
 $pdf = new FPDF();
 $pdf->AddPage();
 $pdf->SetFont('Arial','B',16);
-$pdf->Cell(40,10,'Hello World!');
+$pdf->Cell(40,10,$d);
 $pdf->Output();
-
+*/
 ?>
