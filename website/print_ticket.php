ed2af4561e95f98894275373455ac8ae1018dda6
Syed Ahmad <Ahmad@eduroam-int-dhcp-97-222-46.ucl.ac.uk>
2017-12-13 14:52:00 +0000

    Added SQL Query

--- website/print_ticket.php
+++ website/print_ticket.php
@@ -1,8 +1,19 @@
 <?php
-
+require 'function.php';
 require('libraries/fpdf/fpdf.php');
 $ticket_ID = $_COOKIE["ticket"];
 
+$conn = db_connect();
+
+$sql = "SELECT events.Name, events.Start_DateTime, venue.Name AS VenueName
+        FROM events
+        JOIN tickets ON events.Event_ID=tickets.Event_ID
+        JOIN venue ON events.Venue_ID=venue.Venue_ID
+        WHERE Ticket_ID = $ticket_ID;";
+
+$result = mysqli_query($conn, $sql);
+$row = mysqli_fetch_assoc($result);
+mysqli_close($conn);
 
 $pdf = new FPDF();
 $pdf->AddPage();
@@ -10,5 +21,4 @@ $pdf->SetFont('Arial','B',16);
 $pdf->Cell(40,10,'Hello World!');
 $pdf->Output();
 
-
- ?>
+?>
