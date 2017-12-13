ac3d09201f257e7b61b23d512c6ae347df6ab2ff
eugeneval <eugval@gmail.com>
2017-12-13 14:04:39 +0000

    working on printing tickets

--- website/print_ticket.php
+++ website/print_ticket.php
@@ -1,12 +1,8 @@
 <?php
 
-<<<<<<< HEAD
 require('libraries/fpdf/fpdf.php');
 $ticket_ID = $_COOKIE["ticket"];
 
-=======
-require('fpdf.php');
->>>>>>> 228e75dda6d2001ffb4bff23cbbd27f909a04e60
 
 $pdf = new FPDF();
 $pdf->AddPage();
