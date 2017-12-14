8b53fbe67a0d7f1249e7179e69688fe360221d9b
eugeneval <eugval@gmail.com>
2017-12-14 16:46:25 +0000

    cannot input invalid dates

--- website/javascript/event_creation.js
+++ website/javascript/event_creation.js
@@ -6,3 +6,36 @@ function newVenue() {
     }
 
 }
+
+function checkTimes() {
+    tStart = new Date(document.getElementById('ticket_start').valueAsDate);
+    tEnd = new Date(document.getElementById('ticket_end').valueAsDate);
+    eStart = new Date(document.getElementById('event_start').valueAsDate);
+    eEnd = new Date(document.getElementById('event_end').valueAsDate);
+    now = new Date();
+
+
+    if (tStart >= tEnd) {
+        alert("You cannot start your event after it ends!");
+        return false;
+    } else if (eStart >= eEnd) {
+        alert("You cannot start selling tickets after you stop selling tickets!");
+        return false;
+    } else if (tStart >= eStart) {
+        alert("You cannot start selling tickets after the event starts!");
+        return false;
+    } else if (tEnd >= eEnd) {
+        alert("You cannot stop selling tickets after yiur event ends!");
+        return false;
+    } else if (eEnd >= now) {
+        alert("You cannot make events in the past!");
+        return false;
+    } else if (tEnd >= now) {
+        alert("You can't finish selling tickets in the past!");
+        return false;
+    } else {
+        return true;
+    }
+
+
+}
