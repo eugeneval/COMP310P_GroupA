b91cb7206d76ea84f3e76da48fcd572a3373700d
eugeneval <eugval@gmail.com>
2017-12-15 16:21:01 +0000

    trying to make it work

--- website/select_interests.php
+++ website/select_interests.php
@@ -23,7 +23,7 @@ $result = mysqli_query($conn, $sql);
      </head>
      <body>
          <header>
-             <a href="main.php"><img src="resources/Logo.png" style="width:302px;height:86px;"/></a>
+             <a href="user_interests.php"><img src="resources/Logo.png" style="width:302px;height:86px;"/></a>
              <h4>Welcome to Eventi, the intelligent assistant for young professionals!</h4>
              <ul>
                  <li class="menubar">Logged in as: <?php echo $username; ?></li>
@@ -35,17 +35,16 @@ $result = mysqli_query($conn, $sql);
              <fieldset>
                  <legend>Categorisation</legend>
                  <p>Select all of your interests for Eventi<br /><br /><small>use ctrl or cmd to select multiple tags</small></p>
-                 <select multiple name="category" size="10">
+                 <select multiple name="category[]" size="10">
                      <option selected="true" disabled="disabled"></option>
                      <?php
-
                          $sql = "SELECT * FROM category";
                          $result = mysqli_query($conn, $sql);
                          while ($row = mysqli_fetch_assoc($result)) {
                              echo "<option value=\"".$row['Category_ID']."\">".$row['Name']."</option>";
                          }
-                         mysqli_close($conn);
-                      ?>
+
+                       ?>
                  </select>
                  <br />
              </fieldset>
